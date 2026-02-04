c459958eec82fad8ce7c992095bd0266f660af3f
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-02-04 19:49:01 +0100

    C backend integration

--- theories/backends/CBackend.v
+++ theories/backends/CBackend.v
@@ -1,5 +1,14 @@
+From MetaRocq.Utils Require Import utils.
 From MetaRocq.Utils Require Import bytestring.
+From MetaRocq.Erasure.Typed Require Import ResultMonad.
+From CertiCoq Require Import Compiler.pipeline.
+From CertiCoq Require Import Common.Pipeline_utils.
 From Peregrine Require Import Config.
+From Peregrine Require Import Utils.
+From Peregrine Require Import CertiCoqBackend.
+From ExtLib.Structures Require Import Monad.
+
+Import MonadNotation.
 
 Local Open Scope bs_scope.
 
@@ -20,3 +29,27 @@ Definition c_phases := {|
   betared'        := Compatible false;
   unboxing'       := Compatible false;
 |}.
+
+
+
+Definition c_pipeline prs (p : EAst.program) :=
+  let env := p.1 in
+  '(prs, next_id) <- register_prims prs next_id env ;;
+  p_anf <- anf_pipeline p prs next_id;;
+  (* Compile lambda_anf to C_light *)
+  p_c <- compile_Clight prs p_anf;;
+  ret p_c.
+
+Definition extract_c (remaps : remappings)
+                     (custom_attr : custom_attributes)
+                     (opts : c_config)
+                     (file_name : string)
+                     (p : EAst.program)
+                     : result Cprogram string :=
+  let config := mk_opts opts in
+  let prs := mk_prims remaps in
+  let (res, _) := run_pipeline EAst.program Cprogram config p (c_pipeline prs) in
+  match res with
+  | compM.Ret p => Ok p
+  | compM.Err s => Err s
+  end.
